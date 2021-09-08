7e57bafe940562c6a541c2c7d134afb3d3b71229
CurtisJohansen <curtis.d.johansen@gmail.com>
2021-09-08 08:49:57 -0400

    added notes

--- wrangle.py
+++ wrangle.py
@@ -84,7 +84,7 @@ def impute_null_values():
 
 # Function to remove outliers in dataframe
 
-  def remove_outliers(df, k, col_list):
+def remove_outliers(df, k, col_list):
     ''' remove outliers from a list of columns in a dataframe 
         and return that dataframe
     '''
@@ -106,7 +106,7 @@ def impute_null_values():
 
 # Function that combines the above functions
 
-    def wrangle_zillow():
+def wrangle_zillow():
     '''
     We will call the other functions from the file in our wrangle_zillow function.
     '''
@@ -121,3 +121,119 @@ def impute_null_values():
 
     #Return DataFrame
     return df
+
+
+############################################################################
+######################### Acquire Telco Function ###########################
+
+#import libraries
+import pandas as pd
+import numpy as np
+import os
+from pydataset import data
+from sklearn.model_selection import train_test_split
+
+# acquire
+import acquire
+
+
+# Create helper function to get the necessary connection url.
+def get_connection(db_name):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    from env import host, user, password
+    return f'mysql+pymysql://{user}:{password}@{host}/{db_name}'
+
+
+#create function to retrieve telco_churn data with specific columns
+def get_telco_data():
+    '''
+    This function reads in the Telco Churn data from the Codeup db
+    and returns a pandas DataFrame with customer_id, monthly_charges, tenure, total_charges columns
+    for customers who have 2-year contracts.
+    '''
+    
+    sql_query = '''
+    SELECT customer_id, monthly_charges, tenure, total_charges
+    FROM customers
+    WHERE contract_type_id LIKE '3'
+    '''
+    return pd.read_sql(sql_query, get_connection('telco_churn'))
+
+############################ ALL Telco Data Function ##############################
+
+#create function to retrieve telco_churn data with all columns
+def all_telco_data(df):
+    '''
+    This function reads in the Telco Churn data from the Codeup db
+    and returns a pandas DataFrame with all columns
+    '''
+    
+    sql_query = '''
+    SELECT *
+    FROM customers
+    '''
+    return pd.read_sql(sql_query, get_connection('telco_churn'))
+
+############################ Wrangle Telco Function ##############################
+
+
+def wrangle_telco():
+    '''
+    This function checks to see if telco_churn.csv already exists, 
+    if it does not, one is created
+    then the data is cleaned and the dataframe is returned
+    '''
+    #check to see if telco_churn.csv already exist
+    if os.path.isfile('telco_churn.csv'):
+        df = pd.read_csv('telco_churn.csv', index_col=0)
+    
+    else:
+
+        #creates new csv if one does not already exist
+        df = get_telco_data()
+        df.to_csv('telco_churn.csv')
+
+    #replace blank spaces and special characters
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+
+    #change total_charges to float from object
+    df.total_charges = df.total_charges.str.replace(' ', '0').astype(float)
+
+    #fill NaN values (10 out of 1695) with monthly charges
+    df.total_charges = df.total_charges.fillna(df.monthly_charges)
+
+    return df
+
+############################ Tenure Years Function ##############################
+
+def months_to_years(df):
+    '''
+    this function accepts the telco churn dataframe
+    and returns a dataframe with a new feature in complete years of tenure
+    '''
+    df['tenure_years'] = df.tenure / 12
+    return df
+
+###########################################################################
+############################ Split Data Function ##############################
+
+def split_data(df):
+    '''
+    split our data,
+    takes in a pandas dataframe
+    returns: three pandas dataframes, train, test, and validate
+    '''
+    #create train_validate and test datasets
+    train, test = train_test_split(df, train_size = 0.8, random_state = 123)
+    #create train and validate datasets
+    train, validate = train_test_split(train, train_size = 0.7, random_state = 123)
+
+    # Have function print datasets shape
+    print(f'train -> {train.shape}')
+    print(f'validate -> {validate.shape}')
+    print(f'test -> {test.shape}')
+   
+    return train, validate, test
